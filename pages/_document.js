7a4670d527df0d03cd770d486032ac3a2af4735e
ivanvb <ivanvalette@hotmail.com>
2022-05-28 21:07:17 -0400

    fix: remove unused fonts

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,10 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
                 <link
                     rel="stylesheet"
-                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                    href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap"
                 />
             </Head>
             <body>
